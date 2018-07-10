17b15736bdf970a69b7131b677f42c0571c08049
jfchu <32548082+jfchu@users.noreply.github.com>
2018-07-10 11:37:24 -0400

    Created new branch for pelod2_calc.R commit
    Master branch will follow score calculation procedure in master file substituting 0's for NA values
    New branch will keep NA scores as NA

--- pelod2_calc.R
+++ pelod2_calc.R
@@ -5,29 +5,38 @@ AgeMonths <- function(age.days) {
 #Global vector of age cutoffs measured in months
 ages <<- c(144, 60, 24, 12, 1)
 
+FindAgeCutoffs <- function(X, age) {
+  if (age >= ages[X]) 
+    return (X)
+  if (age < ages[5])
+    return (6)
+}
+
 #Logical values for score functions -> if clinical value should be less than a certain threshold
 
 #Glasgow Coma Score
 #True
+#Fix score calculation; eg. encid 7 has incorrect score
 GComa <- function(id) {
   cutoffs <- list(c(4, 4), c(10, 1))
-  min.val <- FindExtremeValueMin(id, pelod2.datalist[["GCS"]])
+  min.val <- as.numeric(FindExtremeValueMin(id, pelod2.datalist[["GCS"]]))
   FindCutoffs <- function(X, score) {
     if (is.na(score))
       return (NA)
-    else if (score <= cutoffs[[1]][1])
+    else if (score <= cutoffs[[1]][1]) {
       return (cutoffs[[X]][2])
+    }
     return (0)
   }
-  sapply(seq_len(length(cutoffs)), FindCutoffs, score = min.val)
+  return (max(sapply(seq_len(length(cutoffs)), FindCutoffs, score = min.val)))
 }
 
 #Pupil reactivity should be entered as follows: 1 if true or 0 if false
 #True
 Pupillary <- function(id) {
   cutoffs <- list(c(0, 5))
-  min.val1 <- FindExtremeValueMin(id, pelod2.datalist[["pup.left"]])
-  min.val2 <- FindExtremeValueMin(id, pelod2.datalist[["pup.right"]])
+  min.val1 <- as.numeric(FindExtremeValuePupil(id, pelod2.datalist[["pup.left"]]))
+  min.val2 <- as.numeric(FindExtremeValuePupil(id, pelod2.datalist[["pup.right"]]))
   if (is.na(min.val1) || is.na(min.val2))
     return (NA)
   else if (min.val1 <= cutoffs[[1]][1] && min.val2 <= cutoffs[[1]][1])
@@ -37,14 +46,15 @@ Pupillary <- function(id) {
 
 #Molarity measured in mmol/L
 #False
+#lac.wholeblood contains data with ">" character 
 #!is.numeric(unlist(FindValues(id, pelod2.datalist[["lac.wholeblood"]]), recursive = F))
 Lactatemia <- function(id) {
   cutoffs <- list(c(11.0, 4), c(5.0, 1))
-  max.val1 <- FindExtremeValueMax(id, pelod2.datalist[["lac"]])
-  if (!is.numeric(FindValues(id, pelod2.datalist[["lac.wholeblood"]]))) 
+  max.val1 <- as.numeric(FindExtremeValueMax(id, pelod2.datalist[["lac"]]))
+  if (!is.numeric(as.numeric(FindValues(id, pelod2.datalist[["lac.wholeblood"]])))) 
     return (4)
   else
-    max.val2 <- FindExtremeValueMax(id, pelod2.datalist[["lac.wholeblood"]])
+    max.val2 <- as.numeric(FindExtremeValueMax(id, pelod2.datalist[["lac.wholeblood"]]))
   FindCutoffs <- function(X, molarity) {
     if (is.na(molarity))
       return (NA)
@@ -52,52 +62,41 @@ Lactatemia <- function(id) {
       return (cutoffs[[X]][2])
     return (0)
   }
-  if (is.na(max.val1) || is.na(max.val2))
+  if (is.na(max.val1) && is.na(max.val2))
     return (NA)
-  else if (max.val1 >= max.val2) 
-    sapply(seq_len(length(cutoffs)), FindCutoffs, molarity = max.val1)
+  else if (is.na(max.val2) || (!is.na(max.val1) && max.val1 >= max.val2)) 
+    return (max(sapply(seq_len(length(cutoffs)), FindCutoffs, molarity = max.val1)))
   else
-    sapply(seq_len(length(cutoffs)), FindCutoffs, molarity = max.val2)
+    return (max(sapply(seq_len(length(cutoffs)), FindCutoffs, molarity = max.val2)))
 }
-Lactatemia(1093)
+
 #Age measured in months
 #Mean arterial pressure measure in mm Hg
 #True
 MAP <- function(id, age) {
   cutoffs <- list(list(c(37, 6), c(51, 3), c(66, 2)), list(c(35, 6), c(48, 3), c(64, 2)), list(c(31, 6), c(44, 3), c(61, 2)), list(c(30, 6), c(43, 3), c(59, 2)), list(c(24, 6), c(38, 3), c(54, 2)), list(c(16, 6), c(30, 3), c(45, 2)))
-  min.val <- FindExtremeValueMin(id, pelod2.datalist[["map"]])
-  age.index <- 6
-  FindAgeCutoffs <- function(X, age) {
-    if (age >= ages[X]) 
-      return (X)
-  }
-  age.index <- max(unlist(sapply(seq_len(length(ages)), FindAgeCutoffs, age = age), recursive = F))
+  min.val <- as.numeric(FindExtremeValueMin(id, pelod2.datalist[["map"]]))
+  age.index <- min(unlist(sapply(seq_len(length(ages)), FindAgeCutoffs, age = age), recursive = F))
   FindCutoffs <- function(X, pressure) {
     if (is.na(pressure))
       return (NA)
     else if (pressure <= cutoffs[[age.index]][[X]][1])
-      print(X)
       return (cutoffs[[age.index]][[X]][2])
     return (0)
   }
-  sapply(seq_len(length(cutoffs[[1]])), FindCutoffs, pressure = min.val)
+  return (max(sapply(seq_len(length(cutoffs[[1]])), FindCutoffs, pressure = min.val)))
 }
 
 #Age measured in months
 #Molarity measured in μmol/L
 #False
+#Conversion factor from raw data to units used in score calculation is 88.44
+#cr contains data with "<" character
+#max never selects the specific non-numeric data in this sheet but a workaround would be ideal
 Creatinine <- function(id, age) {
   cutoffs <- list(list(c(93, 2)), list(c(59, 2)), list(c(51, 2)), list(c(35, 2)), list(c(23, 2)), list(c(70, 2)))
-  ReplaceNaN <- function()
-  if (!is.numeric(FindValues(id, pelod2.datalist[["cr"]])))
-    
-  max.val <- FindExtremeValueMax(id, pelod2.datalist[["cr"]])
-  age.index <- 6
-  FindAgeCutoffs <- function(X, age) {
-    if (age > ages[X])
-      return (X)
-  }
-  age.index <- max(unlist(sapply(seq_len(length(ages)), FindAgeCutoffs, age = age), recursive = F))
+  max.val <- as.numeric(FindExtremeValueMax(id, pelod2.datalist[["cr"]])) * 88.44
+  age.index <- min(unlist(sapply(seq_len(length(ages)), FindAgeCutoffs, age = age), recursive = F))
   FindCutoffs <- function(X, cr.molarity) {
     if (is.na(cr.molarity))
       return (NA)
@@ -105,7 +104,7 @@ Creatinine <- function(id, age) {
       return (cutoffs[[age.index]][[X]][2])
     return (0)
   }
-  sapply(seq_len(length(cutoffs[[1]])), FindCutoffs, cr.molarity = max.val)
+  return (max(sapply(seq_len(length(cutoffs[[1]])), FindCutoffs, cr.molarity = max.val)))
 }
 
 #Carrico index = PaO2(measured in mm Hg)/FIO2
@@ -114,8 +113,8 @@ Creatinine <- function(id, age) {
 #True
 Carrico <- function(id) {
   cutoffs <- list(c(60, 2))
-  min.val <- FindExtremeValueMin(id, pelod2.datalist[["pao2"]])
-  max.val <- FindExtremeValueMax(id, pelod2.datalist[["fio2"]])
+  min.val <- as.numeric(FindExtremeValueMin(id, pelod2.datalist[["pao2"]]))
+  max.val <- as.numeric(FindExtremeValueMax(id, pelod2.datalist[["fio2"]]))
   if (is.na(min.val) || is.na(max.val))
     return (NA)
   else if (min.val / max.val <= cutoffs[[1]][1])
@@ -125,24 +124,27 @@ Carrico <- function(id) {
 
 #PaCO2(measured in mm HG) -> partial pressure arterial carbon dioxide
 #True
+#paco2 contains data with ">" character
 PaCO2 <- function(id) {
   cutoffs <- list(c(95, 3), c(59, 1))
-  min.val <- FindExtremeValueMin(id, pelod2.datalist[["paco2"]])
+  if (!is.numeric(as.numeric(FindValues(id, pelod2.datalist[["paco2"]]))))
+    return (3)
+  max.val <- as.numeric(FindExtremeValueMax(id, pelod2.datalist[["paco2"]]))
   FindCutoffs <- function(X, partial.pressure) {
     if (is.na(partial.pressure))
       return (NA)
-    else if (partial.pressure <= cutoffs[[X]][1])
+    else if (partial.pressure >= cutoffs[[X]][1])
       return (cutoffs[[X]][2])
     return (0)
   }
-  sapply(seq_len(length(cutoffs)), FindCutoffs, partial.pressure = min.val)
+  return (max(sapply(seq_len(length(cutoffs)), FindCutoffs, partial.pressure = max.val)))
 }
 
 #Presence of invasive ventilation should be entered as follows: 1 if true or 0 if false
 #False
 Ventilation <- function(id) {
   cutoffs <- list(c(1, 3))
-  max.val <- FindExtremeValueVent(id, pelod2.datalist[["vent"]])
+  max.val <- as.numeric(FindExtremeValueVent(id, pelod2.datalist[["vent"]]))
   if (is.na(max.val))
     return (NA)
   else if (max.val >= cutoffs[[1]][1])
@@ -152,9 +154,13 @@ Ventilation <- function(id) {
 
 #wbc.count * 10^9/L
 #True
+#wbc contains data with "<" character 
+#Fix this; all encid's have score of 2
 WBC <- function(id) {
   cutoffs <- list(c(2, 2))
-  min.val = FindExtremeValueMin(id, pelod2.datalist[["wbc"]])
+  if (!is.numeric(FindValues(id, pelod2.datalist[["wbc"]])))
+    return (2)
+  min.val = as.numeric(FindExtremeValueMin(id, pelod2.datalist[["wbc"]]))
   if (is.na(min.val))
     return (NA)
   else if (min.val <= cutoffs[[1]][1])
@@ -166,7 +172,7 @@ WBC <- function(id) {
 #True
 Platelet <- function(id) {
   cutoffs <- list(c(76, 2), c(141, 1))
-  min.val = FindExtremeValueMin(id, pelod2.datalist[["plate"]])
+  min.val = as.numeric(FindExtremeValueMin(id, pelod2.datalist[["plate"]]))
   FindCutoffs <- function(X, plate.count) {
     if (is.na(plate.count))
       return (NA)
@@ -174,7 +180,7 @@ Platelet <- function(id) {
       return (cutoffs[[X]][2])
     return (0)
   }
-  sapply(seq_len(length(cutoffs)), FindCutoffs, plate.count = min.val)
+  return (max(sapply(seq_len(length(cutoffs)), FindCutoffs, plate.count = min.val)))
 }
 
 ProbMortality <- function(pelod2) {
@@ -197,8 +203,11 @@ FindExtremeValueMin <- function(id, frame) {
 
 FindExtremeValuePupil <- function(id, frame) {
   vals <- frame[which(frame$encid == id), "Clinical.Event.Result"]
-  FilterNonreactive <- function(X) {if (X == "Nonreactive") return (0) else return (1)}
-  return (min(unlist(sapply(vals, FilterNonreactive), recursive = F)))
+  FilterNonreactive <- function(X) {if (X == "Nonreactive") return (0) else if (X == "Unable to assess") return (NA) else return (1)}
+  if (all(is.na(sapply(vals, FilterNonreactive))))
+    return (NA)
+  else
+    return (min(unlist(sapply(vals, FilterNonreactive), recursive = F), na.rm = T))
 }
 
 FindExtremeValueVent <- function(id, frame) {
@@ -212,15 +221,25 @@ PELOD2Scores <- function(frame.list) {
     pelod2.id = read.xlsx("PELOD2_calculator_deid_unencrypt.xlsx")$encid,
     pelod2.age = AgeMonths(read.xlsx("admit1picu_deid_unencrypt.xlsx")$AGEDAYS))
     pelod2.frame$pelod2.gcs = sapply(pelod2.frame$pelod2.id, GComa)
+    print("gcs")
     pelod2.frame$pelod2.pup = sapply(pelod2.frame$pelod2.id, Pupillary)
-    pelod2.frame$pelod2.lac = sapply(pelod2.frame$pelod2.id, Pupillary)
+    print("pup")
+    pelod2.frame$pelod2.lac = sapply(pelod2.frame$pelod2.id, Lactatemia)
+    print("lac")
     pelod2.frame$pelod2.map = mapply(MAP, pelod2.frame$pelod2.id, pelod2.frame$pelod2.age)
+    print("map")
     pelod2.frame$pelod2.cr = mapply(Creatinine, pelod2.frame$pelod2.id, pelod2.frame$pelod2.age)
-    pelod2.frame$pelod2.carrico = sapply(pelod2.frame$pelod2.id, Carrico) 
+    print("cr")
+    pelod2.frame$pelod2.carrico = sapply(pelod2.frame$pelod2.id, Carrico)
+    print("carrico")
     pelod2.frame$pelod2.paco2 = sapply(pelod2.frame$pelod2.id, PaCO2)
+    print("paco2")
     pelod2.frame$pelod2.vent = sapply(pelod2.frame$pelod2.id, Ventilation)
+    print("vent")
     pelod2.frame$pelod2.wbc = sapply(pelod2.frame$pelod2.id, WBC)
+    print("wbc")
     pelod2.frame$pelod2.plate = sapply(pelod2.frame$pelod2.id, Platelet)
+    print("plate")
     pelod2.frame$pelod2.neuroscores = pelod2.frame$pelod2.gcs + pelod2.frame$pelod2.pup
     pelod2.frame$pelod2.cardioscores = pelod2.frame$pelod2.lac + pelod2.frame$pelod2.map
     pelod2.frame$pelod2.renalscores = pelod2.frame$pelod2.cr
@@ -265,7 +284,7 @@ View(PELOD2Scores(pelod2.datalist))
 #print (pelod2.datalist[[1]]["Clinical.Event.Performed.Date/Time"])
 
 #Issues to consider:
-#1. Handling non-numeric data
-#Non-numeric values in Lactate Whole Blood(>), Creatine(<), PaCO2(>), WBC(<) 
+#1. Handling non-numeric data (finished except for creatinine)
+#Non-numeric values in Lactate Whole Blood(>), Creatinine(<), PaCO2(>), WBC(<) 
 #2. Unlisting list containing character and numeric data (coercing numeric data to character)
-#3. Creatine vs. creatinine data value differences in different files
+#3. Creatine vs. creatinine data value differences in different files (finished)
